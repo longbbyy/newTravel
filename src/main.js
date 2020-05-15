c8b803acb77cd084e1ead3f73420ba424287638f
longbbya <lwj15055161078@163.com>
2020-05-15 16:29:21 +0800

    首页轮播

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import '@/assets/style/reset.css'
 import '@/assets/style/border.css'
 import '@/assets/style/iconfont.css'
+import 'swiper/css/swiper.css'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAwesomeSwiper)
 
 new Vue({
   router,
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="home">
     <home-head></home-head>
+    <home-swiper></home-swiper>
   </div>
 </template>
 
 <script>
 import homeHead from './components/homeheade.vue'
+import homeSwiper from './components/swiper.vue'
 export default {
-  components: { homeHead }
+  components: { homeHead, homeSwiper }
 }
 </script>
 
