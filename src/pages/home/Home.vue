cfe5a90b8645b21c25d7d54bd779f8dc63eabf1c
longbbya <lwj15055161078@163.com>
2020-05-18 10:54:51 +0800

    finish Icon

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,14 +2,16 @@
   <div class="home">
     <home-head></home-head>
     <home-swiper></home-swiper>
+    <home-icons></home-icons>
   </div>
 </template>
 
 <script>
 import homeHead from './components/homeheade.vue'
 import homeSwiper from './components/swiper.vue'
+import homeIcons from './components/homeicons.vue'
 export default {
-  components: { homeHead, homeSwiper }
+  components: { homeHead, homeSwiper, homeIcons }
 }
 </script>
 
